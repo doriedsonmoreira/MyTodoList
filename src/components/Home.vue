61ed6f1847c5cf365a3cd8f0539ead05e13bd4a9
Doriedson Moreira <doriedsonpessoa247@gmail.com>
2026-03-12 13:52:17 -0300

    feat(mytodolist):Added new features
    have been added, the home page design is completed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,16 +3,29 @@
     class="bg-[#7B37FA] h-screen row-auto content-center justify-items-center"
   >
     <div
-      class="rounded-[11px] bg-white w-[1200px] h-[800px] justify-items-center"
+      class="p-[65px] grid rounded-[11px] bg-white w-[1200px] h-[800px] justify-items-center items-center"
     >
       <h1 class="text-[35px] text-[#585F7A]">MY TODO-LIST</h1>
-      <div class="flex">
+      <div class="flex gap-[275px]">
         <TaskCounting :variant="1" counting="0" />
         <TaskCounting :variant="2" counting="0" />
         <TaskCounting :variant="3" counting="0" />
       </div>
       <AddTask />
-      <Task date="12-03-2026" />
+      <div class="grid gap-[15px]">
+        <Task date="12-03-2026" :priority="1" :status="1" />
+        <Task date="12-03-2026" :priority="2" :status="2" />
+        <Task date="12-03-2026" :priority="3" :status="3" />
+        <Task date="12-03-2026" :priority="4" :status="3" />
+      </div>
+      <div>
+        <Button
+          variant="outline"
+          class="bg-[#FA3737] text-white text-[20px] p-[11px] border-transparent w-[200px] h-[50px]"
+        >
+          Delete All Tasks
+        </Button>
+      </div>
     </div>
   </section>
 </template>
@@ -21,4 +34,5 @@
 import AddTask from "./AddTask.vue";
 import Task from "./Task.vue";
 import TaskCounting from "./TaskCounting.vue";
+import Button from "./ui/button/Button.vue";
 </script>
--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,27 +1,25 @@
 <template>
-  <div class="flex bg-[#585F7A] rounded-[11px] w-[950px] h-[60px]">
-    <Item>
-      <ItemContent>
-        <ItemTitle class="text-white text-[20px]"
-          >Lorem ipsum dolor sit amet</ItemTitle
-        >
-      </ItemContent>
+  <div
+    class="flex bg-[#585F7A] rounded-[11px] w-[950px] h-[60px] justify-between px-[20px]"
+  >
+    <ItemTitle class="text-white text-[20px]"
+      >Lorem ipsum dolor sit amet</ItemTitle
+    >
+    <div class="flex gap-[50px] select-none">
       <div class="flex">
-        <div class="flex">
-          <PriorityAndStatus :variant="4" />
-          <Status :variant="1" />
-        </div>
-        <ItemActions>
-          <p class="text-white text-[15px]">{{ date }}</p>
-          <button>
-            <img src="../assets/eye.svg" alt="" />
-          </button>
-          <button>
-            <img src="../assets/trash.svg" alt="" />
-          </button>
-        </ItemActions>
+        <Priority :variant="priority" />
+        <Status :variant="status" />
       </div>
-    </Item>
+      <ItemActions class="gap-[20px]">
+        <p class="text-white text-[15px]">{{ date }}</p>
+        <button class="cursor-pointer">
+          <img src="../assets/eye.svg" alt="viewButton" />
+        </button>
+        <button class="cursor-pointer">
+          <img src="../assets/trash.svg" alt="deleteButton" />
+        </button>
+      </ItemActions>
+    </div>
   </div>
 </template>
 
@@ -32,11 +30,13 @@ import {
   ItemContent,
   ItemTitle,
 } from "@/components/ui/item";
-import PriorityAndStatus from "./Priority.vue";
+import Priority from "./Priority.vue";
 import Status from "./Status.vue";
 import { defineProps } from "vue";
 
 defineProps({
   date: String,
+  priority: Number,
+  status: Number,
 });
 </script>
